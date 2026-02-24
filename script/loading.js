21f7577f5f6667c95a8317d9becf7e4b2e857f32
henryheap <henrykaimheap@gmail.com>
2026-02-24 18:18:18 +0000

    Tue, Feb 24, 2026, 10:18 AM -08:00

--- script/loading.js
+++ script/loading.js
@@ -3,6 +3,8 @@ bg = document.querySelector('.bg')
 
 load = 0
 
+int = setInterval(blurring, 30)
+
 loadText.innerText = `${load}%`
 loadText.style.opacity = 1-load/100
 bg.style.filter = `blur(${50-load/2}px)`
