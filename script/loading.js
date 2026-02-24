55a5abcf83a8eb08ff5b5701b30a0451d03b737c
henryheap <henrykaimheap@gmail.com>
2026-02-24 18:22:29 +0000

    Tue, Feb 24, 2026, 10:22 AM -08:00

--- script/loading.js
+++ script/loading.js
@@ -8,7 +8,11 @@ int = setInterval(blurring, 30)
 function blurring() {
     load = load + 1
     loadText.innerText = `${load}%`
-    // loadText.style.opacity = 1-load/100
+    loadText.style.opacity = 1-load/100
     bg.style.filter = `blur(${50-load/2}px)`
+
+    if (load > 99) {
+        clearInterval(int)
+    }
 }
 
