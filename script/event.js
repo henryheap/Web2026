7885f929a7358b7d2ec309691ff1a40bb8d3b4ec
henryheap <henrykaimheap@gmail.com>
2026-03-06 17:38:25 +0000

    Fri, Mar 6, 2026, 9:38 AM -08:00

--- script/event.js
+++ script/event.js
@@ -1,4 +1,8 @@
 clickBox = document.querySelector('.click')
+spinBox = document.querySelector('.spin')
+moveBox = document.querySelector('.move')
+hoverBox = document.querySelector('.hover')
+dblclickBox = document.querySelector('.dblclick')
 
 colors = ['blue', 'green', 'teal', 'red']
 index = 0
@@ -6,9 +10,48 @@ index = 0
 clickBox.addEventListener('click', () => {
     clickBox.style.background = colors[index]
     index = index + 1
-    if (index == colors.length){
+    if (index == colors.length) {
         index = 0
     }
     clickBox.style.color = 'white'
 })
 
+
+spinBox.addEventListener('click', () => {
+    spinBox.classList.toggle('start')
+})
+
+
+x = 0
+y = 0
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight') {
+        x = x + 10;
+    }
+    if (event.key == 'ArrowUp') {
+        y = y - 10
+    }
+    if (event.key == 'ArrowLeft') {
+        x = x - 10
+    }
+    if (event.key == 'ArrowDown') {
+        y = y + 10
+    }
+    moveBox.style.transform = `translate(${x}px, ${y}px)`
+})
+
+
+hoverBox.addEventListener('mouseenter', () => {
+    hoverBox.style.transform = "scale(0.5)"
+})
+
+hoverBox.addEventListener('mouseleave', () => {
+    hoverBox.style.transform = "scale(1)"
+
+})
+
+
+dblclickBox.addEventListener("dblclick", () => {
+    document.body.style.background = 'white'
+})
