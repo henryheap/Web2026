e0c2bb7826f7c40086fd113e54942ef282b69972
henryheap <henrykaimheap@gmail.com>
2026-03-09 10:03:25 -0700

    askdhfljk

--- script/interactive.js
+++ script/interactive.js
@@ -1,6 +1,15 @@
 clickBox = document.querySelector('body')
 
+colors = ['blue', 'teal', 'white', 'red']
+index = 0
+
 clickBox.addEventListener('click', () => {
-    clickBox.classList.toggle("bg")
+    clickBox.style.background = colors[index]
+    index = index + 1
+    if (index == colors.length) {
+        index = 0
+    }
+    clickBox.style.color = 'black'
 })
+
 
