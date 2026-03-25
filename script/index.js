06fd1ad7da98fcc9141d975cc55db1ef43a47a6d
henryheap <henrykaimheap@gmail.com>
2026-03-25 17:30:18 +0000

    Wed, Mar 25, 2026, 10:30 AM -07:00

--- script/index.js
+++ script/index.js
@@ -1,1 +1,14 @@
+container = document.querySelctor('.container')
 
+
+int = setInterval(blurring, 30)
+
+function blurring() {
+
+ const backgroundElement = document.getElementById('.container');
+    if (backgroundElement) {
+        backgroundElement.classList.remove('blurred-background');
+        backgroundElement.classList.add('unblurred-background');
+    }
+
+}
